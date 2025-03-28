88a8444fe15dcf90581327ca97ba2b4e287ef04e
David Arabuli <davidsarabuli@gmail.com>
2025-03-28 15:17:38 +0200

    fixed sorting and filtering (keeps tag filter  when changing sorting\pages)

--- resources/views/reviews/index.blade.php
+++ resources/views/reviews/index.blade.php
@@ -5,6 +5,9 @@
     <h1>hello from reviews page</h1>
 
     <form method="GET" action="{{ route('reviews.index') }}">
+        @if(request('tag'))
+        <input type="hidden" name="tag" value="{{ request('tag') }}">
+        @endif
         <label for="sort">Sort by Score:</label>
         <select name="sort" id="sort" onchange="this.form.submit()">
             <option value="desc" {{ request('sort') == 'desc' ? 'selected' : '' }}>Highest First</option>
@@ -27,7 +30,7 @@
         @endforeach
 
         <div>
-            {{$reviews->links()}}
+            {{ $reviews->appends(request()->query())->links() }}
         </div>
     
 </x-layout>
